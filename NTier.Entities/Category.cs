422d493e1400d67ab624012fb0874cef01d78304
ozkanoztas <ozkanoztas438@gmail.com>
2019-05-16 13:21:28 +0300

    Categort class eklendi

---
+++ NTier.Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using NTier.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier.Entities
+{
+    
+   public class Category : CoreEntitiy
+    {
+
+
+        public string  CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+
+    }
+}
