7c00f3909b856bfd73ee694e77b6e4fe25698142
ozkanoztas <ozkanoztas438@gmail.com>
2019-05-16 12:01:50 +0300

    Core katmanına status enum eklendi

---
+++ NTier.Core/Enums/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier.Core.Enums
+{
+    public enum Status
+         
+        
+
+    {
+       none = 0,
+       active = 1,
+       deleted = 2,
+       Passive = 4,
+
+    }
+}
